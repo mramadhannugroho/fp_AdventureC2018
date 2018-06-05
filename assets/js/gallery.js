9be658d233577dc17ce22cb28a57f9d96e9225ea
mramadhanugroho <muhamad.1998@students.amikom.ac.id>
2018-06-05 20:17:21 +0700

    Signed-off-by: mramadhanugroho <muhamad.1998@students.amikom.ac.id> Final Project Pemrograman Web "Mata-mata"

---
+++ assets/js/gallery.js
@@ -0,0 +1,50 @@
+$(document).ready(function(){
+
+    $(".filter-button").click(function(){
+        var value = $(this).attr('data-filter');
+        
+        if(value == "all")
+        {
+            //$('.filter').removeClass('hidden');
+            $('.filter').show('1000');
+        }
+        else
+        {
+//            $('.filter[filter-item="'+value+'"]').removeClass('hidden');
+//            $(".filter").not('.filter[filter-item="'+value+'"]').addClass('hidden');
+            $(".filter").not('.'+value).hide('3000');
+            $('.filter').filter('.'+value).show('3000');
+            
+        }
+    });
+    
+    if ($(".filter-button").removeClass("active")) {
+$(this).removeClass("active");
+}
+$(this).addClass("active");
+
+})
+
+window.onload = function(){ 
+    // Get the modal
+    var modal = document.getElementById('myModal');
+
+    // Get the image and insert it inside the modal - use its "alt" text as a caption
+    var img = document.getElementById('myImg');
+    var modalImg = document.getElementById("img01");
+    var captionText = document.getElementById("caption");
+    img.onclick = function(){
+        modal.style.display = "block";
+        modalImg.src = this.src;
+        captionText.innerHTML = this.alt;
+    }
+
+    // Get the <span> element that closes the modal
+    var span = document.getElementsByClassName("close")[0];
+
+    // When the user clicks on <span> (x), close the modal
+    span.onclick = function() { 
+        modal.style.display = "none";
+    }
+};
+
---
+++ assets/js/scroll.js
@@ -0,0 +1,51 @@
+// Parallaxing + add class active on scroll
+$(document).scroll(function () {
+  
+  // parallaxing
+  var $movebg = $(window).scrollTop() * -0.3;
+  $('.portion').css('background-positionY', ($movebg) + 'px');
+
+  // add class active to nav a on scroll
+  var scrollPos = $(document).scrollTop() + 100;
+  // changing padding of nav a on scroll
+    if (scrollPos > 250) {
+      $('nav a').addClass('small');
+      $('nav img').addClass('move');
+      $('nav span').removeClass('movetext');
+    } 
+    else {
+      $('nav a').removeClass('small');
+      $('nav img').removeClass('move');
+      $('nav span').addClass('movetext');
+    }
+  
+});
+
+$(document).ready(function(){
+  // Initialize Tooltip
+  $('[data-toggle="tooltip"]').tooltip(); 
+  
+  // Add smooth scrolling to all links in navbar + footer link
+  $("#gotop, #godown").on('click', function(event) {
+
+    // Make sure this.hash has a value before overriding default behavior
+    if (this.hash !== "") {
+
+      // Prevent default anchor click behavior
+      event.preventDefault();
+
+      // Store hash
+      var hash = this.hash;
+
+      // Using jQuery's animate() method to add smooth page scroll
+      // The optional number (900) specifies the number of milliseconds it takes to scroll to the specified area
+      $('html, body').animate({
+        scrollTop: $(hash).offset().top
+      }, 900, function(){
+   
+        // Add hash (#) to URL when done scrolling (default click behavior)
+        window.location.hash = hash;
+      })
+    } // End if
+  })
+});
