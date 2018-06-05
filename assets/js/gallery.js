a248fc0fe5668bb24d7c9612b95dc370992c4f74
mramadhanugroho <muhamad.1998@students.amikom.ac.id>
2018-06-06 00:19:28 +0700

    Signed-off-by: mramadhanugroho <muhamad.1998@students.amikom.ac.id>

--- assets/js/gallery.js
+++ assets/js/gallery.js
@@ -19,32 +19,16 @@ $(document).ready(function(){
     });
     
     if ($(".filter-button").removeClass("active")) {
-$(this).removeClass("active");
-}
-$(this).addClass("active");
+        $(this).removeClass("active");
+    }
+    $(this).addClass("active");
 
 })
 
-window.onload = function(){ 
-    // Get the modal
-    var modal = document.getElementById('myModal');
+function modal(element) {
+    document.getElementById("img01").src = element.src;
+    document.getElementById("modal01").style.display = "block";
+}
 
-    // Get the image and insert it inside the modal - use its "alt" text as a caption
-    var img = document.getElementById('myImg');
-    var modalImg = document.getElementById("img01");
-    var captionText = document.getElementById("caption");
-    img.onclick = function(){
-        modal.style.display = "block";
-        modalImg.src = this.src;
-        captionText.innerHTML = this.alt;
-    }
 
-    // Get the <span> element that closes the modal
-    var span = document.getElementsByClassName("close")[0];
-
-    // When the user clicks on <span> (x), close the modal
-    span.onclick = function() { 
-        modal.style.display = "none";
-    }
-};
 
